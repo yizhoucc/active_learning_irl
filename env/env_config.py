0d8b2083817f042d51b95ad58599ef4fa4b04a74
yizhoucc <yizhoucc@gmail.com>
2023-03-29 20:03:01 -0500

    task

---
+++ env/env_config.py
@@ -0,0 +1,33 @@
+# this file is for the collection of parameters
+
+# this is for log variance noise parameter
+import numpy as np
+from numpy import pi
+
+class Config:
+    def __init__(self):
+        # timeout
+        self.timeout=500 # timesteps
+
+        # -------------- param range ---------------------------
+        self.gravity_range = [1,100]#9.8,
+        self.masscart_range = [0.1,10]#1.0,
+        self.masspole_range = [0.01,3]#0.1,
+        self.length_range = [0.1,3]#0.5,  # actually half the pole's length
+        self.force_mag_range = [1,100]#10.0
+        self.tau=0.02
+        # -------------- param sampling ----------------------
+        self.reso = 15
+        self.sample_method='log'
+
+        # fixed
+        self.gravity = None
+        self.masscart = 1
+        self.masspole = 0.1
+        self.length = None  # actually half the pole's length
+        self.force_mag = 10
+
+
+
+
+
